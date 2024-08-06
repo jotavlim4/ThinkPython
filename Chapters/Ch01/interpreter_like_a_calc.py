5fddc62eae0fcb33691847bce558a1850a532fc1
João Victor <jotav.lim4@gmail.com>
2024-08-06 20:25:51 -0300

    Solução exercício 2, ch1

---
+++ Chapters/Ch01/interpreter_like_a_calc.py
@@ -0,0 +1,12 @@
+# quantos segundos há em 42 minutos e 42 segundos?
+print('quantidade de segundos em 42min e 42s:', 42*60 + 42*1) #2562
+
+# Quantas milhas há em 10 km?
+# 1 milha -> 1,61 km, logo x milhas = y/1,61
+print('quantidade de milhas em 10km:', 10/1.61) #6.211180124223602
+
+# pace médio se a distancia é 10km e o tempo decorrido é 00h42min42s?
+# velocidade media em milhas por hora?
+print('pace medio (minutos):', (42 + 42/60)/6.21)
+print('pace medio (segundos):', 2562/6.21)
+print('velocidade media (milhas por hora):', 6.21/(2562/3600))

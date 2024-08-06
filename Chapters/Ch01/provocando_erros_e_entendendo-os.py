d806af0c3caddfd26e4f22977d8bc866e85ea6d6
João Victor <jotav.lim4@gmail.com>
2024-08-06 20:12:22 -0300

    Solução exercicio 1, Ch1

---
+++ Chapters/Ch01/provocando_erros_e_entendendo-os.py
@@ -0,0 +1,23 @@
+# erro provocado quando usando print('...') sem uma das aspas:
+# print('Erro)
+# provoca o um erro de sintaxe, ou seja, de estrutura.
+# é esperado que uma string seja encerrada por uma aspa simples.
+
+# erro provocado quando usamos print() sem um dos parenteses:
+# print('Hello World!'
+# também provoca um erro de sintaxe. python espera que funcoes iniciem e sejam
+# terminadas com parenteses.
+
+# resultado da soma 2++2. provoca erro de sintaxe? ou entende como a soma do número
+# 2 ao número positivo 2
+# print(2++2)
+# o resultado é o esperado. ele entende como a soma de 2 + (+2)
+
+# zeros em posicões não significativas.
+# print(0o7)
+# retorna um erro de sintaxe avisando que zeros a esquerda nao são permitidos em literais inteiros
+# pede para usar '0o' antes do número para imprimi-lo na base octal.
+
+# dois valores sem nenhum operador entre eles.
+# print(2 2)
+# retorna erro de sintaxe. pergunta se talvez o programador tenha esquecido uma vírgula.
